69b1fc4e66ca63ec3f6461daecc8526b391a312e
Brent Keller <bikeller104@gmail.com>
2022-10-09 14:45:03 -0600

    started implementing the timer and added some coding questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,49 @@
+var timerEl = document.querySelector("#timer")
+var secondsLeft = 75;
+
+//set a default time
+timerEl.textContent = "0";
+
+
+function setTimer()
+{
+    var timerInterval = setInterval(startTimer, 1000);
+}
+
+function startTimer()
+{
+    secondsLeft--;
+    timerEl.TextContent = secondsLeft;
+
+    if(secondsLeft == 0)
+    {
+        clearInterval();
+        //write a function 
+        //to go to the high score screen
+    }
+}
+
+function createStartPage()
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //this object will hold the data for each question
 var QuizQuestion =  {
     questionText: "",
@@ -20,8 +66,74 @@ var QuizAnswer = {
     answerCorrect: false
 }
 
+new QuizAnswer(answer, isCorrect)
+{
+    this.answerText = answer;
+    this.answerCorrect = isCorrect;
+}
 /*
-This js 
-
-
+this array will hold all of the javaScript Questions
+Questions copied from W3 Schools
+    https://www.w3shools.com/quiztest/quiztest.asp?qtest=JS
 */
+var alJSQuestions = [
+    //question 1
+    new QuizQuestion(
+        "Inside which HTML element do we put the JavaScript?",
+        [
+            new QuizAnswer("<js>", false),
+            new QuizAnswer("<scripting>", false),
+            new QuizAnswer("<javascript>", false),
+            new QuizAnswer("script", true)
+        ]
+    ),    
+    //question 2
+    new QuizQuestion(
+        `What is the correct JavaScript syntax to change the content of the HTML element \n
+        <p id="demo"> This is a demonstration. </p>\n ?`,
+        [
+            new QuizAnswer(`#demo.innerHTML = "Hello World!";`,false),
+            new QuizAnswer(`document.getElementByName("p").innerHTML= "Hello World!";`,false),
+            new QuizAnswer(`document.getElement("p").innerHTML = "Hello World!";`,false),
+            new QuizAnswer(`document.getElementById("demo).innerHTML = "Hello World!";`)
+        ]
+    ),
+    //question 3
+    new QuizQuestion (
+        `Where is the correct place to insert a JavaScript?`,
+        [
+            new QuizAnswer(`The <body> section`,true),
+            new QuizAnswer(`Both the <head> section and the <body> section are correct`,false),
+            new QuizAnswer(`The <head> section`,false)
+        ]
+    ),
+    //question 4
+    new QuizQuestion(
+        `What is the correct syntax for refering to an external script called "xxx.js"?`,
+        [
+        new QuizAnswer(`<script href="xxx.js">`, false),
+        new QuizAnswer(`<script src="xxx.js">`, true),
+        new QuizAnswer(`<script name="xxx.js">`, false)
+        ]
+    ),
+    //question 5
+    new QuizQuestion(
+        `The external JavaScript file must contain the <script> tag.`,
+        [
+            new QuizAnswer(`True`, false),
+            new QuizAnswer(`False`, ture)
+        ]
+    ),
+    //skip 6
+    //question 7
+    new QuizQuestion(
+        `How do you create a function in JavaScript?`,
+        [
+            new QuizAnswer(`function:myFunction()`,false),
+            new QuizAnswer(`function = myFunction()`,false),
+            new QuizAnswer(`function myFunction(),`, true)
+        ]
+    )
+]
+
+ 
